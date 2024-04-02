3fb569cfed8714cd6144ccad171e76c3acabc2fd
Jared Teller <17jaredt17@gmail.com>
2024-04-02 01:42:22 -0400

    working commit

--- realMI/app.py
+++ realMI/app.py
@@ -34,76 +34,37 @@ def index():
 # Homes page
 @app.route('/homes')
 def list_homes():
-    # Fetch all cities for the dropdown
     cities = locations_collection.distinct("city")
-
-    # Fetch all owners for the dropdown
     owners = list(owners_collection.find({}, {"_id": 1, "first_name": 1, "last_name": 1}))
-
     home_types = homes_collection.distinct("home_type")
-
-    # for the query in project md
+    for_sale_filter = request.args.get('for_sale') == 'on'
     selected_owner_home_types = request.args.getlist('owner_home_types')
-
-    # Retrieve query parameters
     selected_city = request.args.get('city')
     owner_id = request.args.get('owner_id')
     multiple_sales_filter = request.args.get('multiple_sales') == '1'
-    
     selected_home_types = request.args.getlist('home_types')
-
-    # Construct the query
     query = {}
 
-    # If selected owner home types are provided, adjust the query
     if selected_owner_home_types:
-        # Aggregation pipeline to match owners with all selected home types
-        pipeline = [
-            {"$match": query},  # Apply existing filters
-            {"$group": {
-                "_id": "$owner._id",
-                "home_types": {"$addToSet": "$home_type"},
-                "owner_data": {"$first": "$owner"}
-            }},
-            {"$match": {
-                "home_types": {"$all": selected_owner_home_types}
-            }}
-        ]
+        pipeline = [{"$match": query}, {"$group": {"_id": "$owner._id", "home_types": {"$addToSet": "$home_type"}, "owner_data": {"$first": "$owner"}}}, {"$match": {"home_types": {"$all": selected_owner_home_types}}}]
         owners_with_all_types = list(homes_collection.aggregate(pipeline))
         owner_ids_with_all_types = [owner['_id'] for owner in owners_with_all_types]
-
-        # Adjust the main homes query to include only homes owned by these owners
-        if owner_ids_with_all_types:
-            query["owner._id"] = {"$in": owner_ids_with_all_types}
-        else:
-            query["owner._id"] = None  # No owners match the criteria
+        query["owner._id"] = {"$in": owner_ids_with_all_types} if owner_ids_with_all_types else None
 
     if selected_city:
         query["location.city"] = selected_city
     if owner_id:
         query["owner._id"] = ObjectId(owner_id)
-
-    # If selected home types are provided, adjust the query
     if selected_home_types:
         query["home_type"] = {"$in": selected_home_types}
-
     if multiple_sales_filter:
-        pipeline = [
-            {"$group": {
-                "_id": "$home",
-                "count": {"$sum": 1}
-            }},
-            {"$match": {
-                "count": {"$gt": 1}
-            }}
-        ]
-        multiple_sales = transactions_collection.aggregate(pipeline)
-        home_ids = [sale['_id'] for sale in multiple_sales]
-        query["_id"] = {"$in": home_ids}
+        multiple_sales_homes = [sale['_id'] for sale in transactions_collection.aggregate([{"$group": {"_id": "$home", "count": {"$sum": 1}}}, {"$match": {"count": {"$gt": 1}}}])]
+        query["_id"] = {"$in": multiple_sales_homes}
+    if for_sale_filter:
+        homes_for_sale_ids = [trans['home'] for trans in transactions_collection.find({"buyer": None}, {"home": 1})]
+        query["_id"] = {"$in": homes_for_sale_ids} if homes_for_sale_ids else None
 
-    # Retrieve the filtered list of homes based on the query
     homes = list(homes_collection.find(query))
-
     return render_template('list_homes.html', homes=homes, cities=cities, owners=owners, home_types=home_types, selected_home_types=selected_home_types, selected_owner_home_types=selected_owner_home_types)
 
 # Adding a home form - make sure this is the approach we should be using
